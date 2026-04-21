9d01b7b6994403eb1e913df72cfa47449234a346
soham sangale <sohamdsangale.007@gmail.com>
2026-04-21 11:32:39 +0530

    Add files via upload

---
+++ basicfile.py
@@ -0,0 +1,5 @@
+with open("sample.txt", "w") as file:
+    file.write("Hello, this is the first line in the file.\n")
+    file.write("File handling in Python is easy to learn.\n")
+
+print("Data written successfully.\n")
